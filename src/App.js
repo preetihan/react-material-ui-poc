793fa0dceb717234e862c66b4b2755a18a297223
Preeti Hansukchariya <>
2020-02-06 23:00:38 +0700

    Second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MiniDrawer from './MiniDrawer';
+
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer/>
+    </div>
+  );
+}
+
+export default App;
